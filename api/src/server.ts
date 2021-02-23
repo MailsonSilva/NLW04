067055b196c525638e824f7785a7ce3f0bc0553b
MailsonSilva <mailsonfcsilva@gmail.com>
2021-02-23 13:15:09 -0300

    1 dia-criando api

---
+++ api/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  return res.json({message: "Hello word NLW04"});
+});
+
+app.post('/', (req, res) => {
+
+  return res.json({message: "Dados salvos com sucesso"});
+});
+
+app.listen(3333, () => {
+  console.log("Server is running!");
+});
